dd849d03fc76e3b3e51565dbbd8f7f9af84d8eb7
BigBossJacob <47572661+BigBossJacob@users.noreply.github.com>
2019-03-28 12:05:43 -0400

    Add files via upload

---
+++ assets/sprites/deadtree.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="deadtree" tilewidth="128" tileheight="128" tilecount="2" columns="2">
+ <image source="Assets/Upside Down/upside down files/png/Objects/Tree.png" width="286" height="239"/>
+</tileset>
---
+++ assets/sprites/tree.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tree" tilewidth="128" tileheight="128" tilecount="4" columns="2">
+ <image source="Assets/Overworld/overworld files/png/Object/Tree_2.png" width="282" height="301"/>
+</tileset>
